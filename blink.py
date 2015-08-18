bb2f108565d97c3d72961b0b5c919dbacf1128fb
Alex Kovshovik <Alex.Kovshovik@plansource.com>
2015-08-18 15:22:21 -0600

    Fixed indentation

--- blink.py
+++ blink.py
@@ -6,27 +6,25 @@ import time
 
 LedPin = 11
 
-ApiHeaders = { 'Content-Type': 'application/json', 'Accept': 'application/json' }
-
 def setup():
-	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(LedPin, GPIO.OUT)
-	GPIO.output(LedPin, GPIO.HIGH)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(LedPin, GPIO.OUT)
+    GPIO.output(LedPin, GPIO.HIGH)
 
 def loop():
-	while True:
-	    GPIO.output(LedPin, GPIO.LOW)
-		time.sleep(0.5)
-	    GPIO.output(LedPin, GPIO.HIGH)
-		time.sleep(0.5)
+    while True:
+        GPIO.output(LedPin, GPIO.LOW)
+        time.sleep(0.5)
+        GPIO.output(LedPin, GPIO.HIGH)
+        time.sleep(0.5)
 
 def destroy():
-	GPIO.output(LedPin, GPIO.HIGH)
-	GPIO.cleanup()
+    GPIO.output(LedPin, GPIO.HIGH)
+    GPIO.cleanup()
 
 if __name__ == '__main__':
-	setup()
-	try:
-		loop()
-	except KeyboardInterrupt:
-		destroy()
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
