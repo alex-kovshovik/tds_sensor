b4b65f0266f5b1c5230ce3853ae62c952a98093e
Alex Kovshovik <Alex.Kovshovik@plansource.com>
2015-08-21 11:25:27 -0600

    Implemented support for multiple toilets on different pins

--- sensor.py
+++ sensor.py
@@ -1,61 +1,62 @@
 #!/usr/bin/env python
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
 
-import RPi.GPIO as GPIO
+import RPiMock.GPIO as GPIO
 import time
 import requests
 import json
 
 from requests.exceptions import ConnectionError
 
+# NOTE: override this to add more toilets!
+SensorPins = { 13: 1 } # Keys are GPIO pin numbers, values are toilet IDs
+
 LedPin = 11
-SenPin = 13
 
 ApiHeaders = { 'Content-Type': 'application/json', 'Accept': 'application/json' }
 
 def setup():
 	GPIO.setmode(GPIO.BOARD)
 	GPIO.setup(LedPin, GPIO.OUT)
-	GPIO.setup(SenPin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-
 	GPIO.output(LedPin, GPIO.HIGH)
 
-# TODO: TO BE FINISHED!
-def blinkLed():
-	while True:
-		GPIO.output(LedPin, GPIO.LOW)
-		time.sleep(0.2)
-		GPIO.output(LedPin, GPIO.HIGH)
-		time.sleep(0.2)
+	for pin in SensorPins:
+		GPIO.setup(pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
-def update_toilet_state(is_closed):
+def update_toilet_state(toilet_id, is_closed):
 	new_state = 'occupied' if is_closed else 'available'
 
 	try:
 		update_params = { 'toilet': { 'state' : new_state } }
-		r = requests.patch('http://tds.shovik.com/toilets/1', data = json.dumps(update_params), headers = ApiHeaders)
+		r = requests.patch('http://tds.shovik.com/toilets/' + str(toilet_id), data = json.dumps(update_params), headers = ApiHeaders)
 
-		print 'Updated to "' + new_state + '". Status: ' + str(r.status_code)
+		print 'Updated toilet ID "' + str(toilet_id) + ' to "' + new_state + '". Status: ' + str(r.status_code)
 	except ConnectionError as e:
 		print e
 
-def update_state(was_closed, is_closed):
+def update_state(pin, was_closed, is_closed):
+	toilet_id = SensorPins[pin]
+
 	if not was_closed and is_closed:
 		GPIO.output(LedPin, GPIO.LOW)
-		update_toilet_state(is_closed)
+		update_toilet_state(toilet_id, is_closed)
 
 	if was_closed and not is_closed:
 		GPIO.output(LedPin, GPIO.HIGH)
-		update_toilet_state(is_closed)
+		update_toilet_state(toilet_id, is_closed)
 
 	return is_closed
 
 def loop():
-	was_closed = False
+	was_closed = {} # Keys are GPIO pin numbers, values are boolean previous state.
+	for pin, toilet_id in enumerate(SensorPins):
+		was_closed[pin] = False
 
 	while True:
-		time.sleep(1) # Every second is frequent enough, IMHO.
-		was_closed = update_state(was_closed, GPIO.input(SenPin) == GPIO.LOW)
+		time.sleep(0.2)
+
+		for pin, toilet_id in enumerate(SensorPins):
+			was_closed[pin] = update_state(pin, was_closed[pin], GPIO.input(pin) == GPIO.LOW)
 
 def destroy():
 	GPIO.output(LedPin, GPIO.HIGH)
